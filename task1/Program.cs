35e77e7d9b4370c7e11a41ebc09c70aa63a923f2
Sergey <babarin2607@rambler.ru>
2023-11-07 02:46:38 +0500

    Первая задача/2

--- task1/Program.cs
+++ task1/Program.cs
@@ -4,3 +4,29 @@
     int result = Convert.ToInt32(value);
     return result;
 }
+int[] InputArray(int lenght) {
+    int[] array = new int [lenght];
+    for(int i = 0; i < array.Length; i++) {
+        array[i] = Prompt($"Введите {i + 1}-й элемент");
+    }
+    return array;
+}
+void PrintArray(int[] array) {
+    for (int i = 0; i < array.Length; i++){
+        Console.WriteLine($"а [{i}] = {array[i]}");
+    }
+}
+int CountPositiveNumbers(int[] array){
+    int count = 0;
+    for (int i = 0; i < array.Length; i++){
+        if (array[i] > 0){
+            count++;
+        }
+    }
+    return count;
+}
+int Length = Prompt("Введите количество элементов >");
+int[] array;
+array = InputArray(Length);
+PrintArray(array);
+Console.WriteLine($"Количество чисел больше 0 - {CountPositiveNumbers(array)}");
