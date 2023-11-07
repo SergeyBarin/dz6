4118d7c5a39607cbb35b6e68009ab9e47b2cec8e
Sergey <babarin2607@rambler.ru>
2023-11-07 14:03:33 +0500

    Вторая задача

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Reflection.Metadata;
+
+const int COEFFICIENT = 0;
+const int CONSTANT = 1;
+const int X_COORD = 0;
+const int Y_COORD = 1;
+const int LINE1 = 1;
+const int LINE2 = 2;
+
+double[] lineData1 = InputLineData(LINE1);
+double[] lineData2 = InputLineData(LINE2);
+
+if (ValidateLines(lineData1, lineData2)){
+    double[] coord = FindCoords(lineData1, lineData2);
+    Console.Write($"Точка пересечения уравнений у = {lineData1[COEFFICIENT]}*x+{lineData2[CONSTANT]} и у = {lineDate2[COEFFICIENT]}*x+{lineData2[CONSTANT]} ");
+    Console.WriteLine($" имеет координаты ({coord[X_COORD]}, {coord[Y_COORD]})");
+}
+double Prompt(string message){
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    double result = Convert.ToDouble(value);
+    return result;
+}
+double[] InputLineData(int nubmerOfLine){
+    double[] lineData = new double[2];
+    lineData[COEFFICIENT] = Prompt($"Введите коэффициент для {nubmerOFLine} прямой > ");
+    lineData[Constant] = Prompt($"Введите константу для {numberOFLine} прямой >");
+    return lineData;
+}
+double[] FindCoords(double[] lineData1, double[] lineData2){
+    double[] coord = new double[2];
+    coord[X_COORD] = (lineData1[CONSTANT] - lineData2[CONSTANT]) / (lineData2[COEFFICIENT] - lineData1[COEFFICIENT]);
+    coord[Y_COORD] = lineData1[CONSTANT] * coord[X_COORD] + lineData1[CONSTANT];
+    return coord;
+}
+bool ValidateLines(double[] lineData1, double[] lineData2){
+    if(lineData1[COEFFICIENT] == lineData2[COEFFICIENT]){
+        if(lineData1[CONSTANT] == lineData2[CONSTANT]){
+            Console.WriteLine("Прямые совпадают");
+            return false;
+        }
+        else{
+            Console.WriteLine("Прямые параллельны");
+            return false;
+        }
+    }
+    return true;
+}
